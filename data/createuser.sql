2c222357548c03a290cb34992e6dff06bcbf89d7
NathanRetroRabbit <nngobale@retrorabbit.co.za>
2019-01-22 14:29:03 +0200

    Replaced the first name and lastname with my details to create my database and my user entry.
    As well as test it.

--- data/createuser.sql
+++ data/createuser.sql
@@ -1,18 +1,18 @@
 USE [master]
 GO
-CREATE LOGIN [firstnamelastname] WITH PASSWORD=N'rabbit123!@#' MUST_CHANGE, DEFAULT_DATABASE=[sql101.firstname.lastname], CHECK_EXPIRATION=ON, CHECK_POLICY=ON
+CREATE LOGIN [nathanngobale] WITH PASSWORD=N'rabbit123!@#' MUST_CHANGE, DEFAULT_DATABASE=[sql101.nathan.ngobale], CHECK_EXPIRATION=ON, CHECK_POLICY=ON
 GO
-use [sql101.firstname.lastname]
+use [sql101.nathan.ngobale]
 
 GO
 use [master]
 
 GO
-USE [sql101.firstname.lastname]
+USE [sql101.nathan.ngobale]
 GO
-CREATE USER [firstnamelastname] FOR LOGIN [firstnamelastname]
+CREATE USER [nathanngobale] FOR LOGIN [nathanngobale]
 GO
-USE [sql101.firstname.lastname]
+USE [sql101.nathan.ngobale]
 GO
-ALTER ROLE [db_owner] ADD MEMBER [firstnamelastname]
+ALTER ROLE [db_owner] ADD MEMBER [nathanngobale]
 GO
